116a5a06e1aca8f59740aa27854e1be887759301
WebArtixan <umaralihashmi@gmail.com>
2025-09-03 15:44:18 +0500

    first commit

--- js/java.js
+++ js/java.js
@@ -19,3 +19,22 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+
+// Button ko ID se select karein
+const button = document.getElementById('color-change-button');
+
+// Body element ko select karein
+const body = document.body;
+
+// Colors ki ek list banayein
+const colors = ['#3498db', '#e74c3c', '#2ecc71', '#9b59b6', '#f1c40f'];
+
+// Event Listener add karein
+button.addEventListener('click', () => {
+    // Colors array se ek random color select karein
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    
+    // Body ka background-color change karein
+    body.style.backgroundColor = randomColor;
+});
